e984623db1e73e2027bb288d17d37c880dd24d17
contre <rpastric@contre.us>
2018-02-27 11:33:59 -0800

    Update to .net462, add automatic generation of nuget packages

--- source/WindowsAPICodePack/ShellExtensions/Properties/AssemblyInfo.cs
+++ source/WindowsAPICodePack/ShellExtensions/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Resources;
 using System.Runtime.InteropServices;
 using System.Security;
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Microsoft.WindowsAPICodePack.ShellExtensions")]
@@ -18,8 +18,8 @@ using System.Security;
 //See: http://msdn.microsoft.com/en-us/library/ee191569.aspx
 [assembly: SecurityRules(SecurityRuleSet.Level1)]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
@@ -29,11 +29,11 @@ using System.Security;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1.3.0")]
